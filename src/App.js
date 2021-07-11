d1adf54c8dcc8213dd2b6877ebfaa3c20116de3d
Rahul <rahulje9@gmail.com>
2021-07-11 14:11:25 +0530

    location fetching added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import Router from "./router/router";
-import { persistor, store } from "./common/store";
-import { PersistGate } from "redux-persist/integration/react";
 import { Provider } from "react-redux";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistor, store } from "./common/store";
+import Router from "./router/router";
 
 function App() {
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <Router />
+        <ToastContainer />
       </PersistGate>
     </Provider>
   );
