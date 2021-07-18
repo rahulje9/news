1816a69737012f414580e9cb5c3a70d8ee728c41
Rahul <rahulje9@gmail.com>
2021-07-18 16:08:33 +0530

    screen renamed

--- src/app/router/router.js
+++ src/app/router/router.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import Home from "../modules/Home";
+import Home from "../modules/HomeScreen";
 import { Redirect } from "react-router-dom";
 
 const Router = () => {
